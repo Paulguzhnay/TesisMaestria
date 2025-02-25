5f7fc88cbc6f9be807e9b3eda992a3d5c77c206c
Paulguzhnay <paul-guzhnay@hotmail.com>
2025-02-25 10:49:48 -0500

    Creacion de modulos y componentes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './features/dashboard/dashboard.component';
+import { SettingsComponent } from './features/settings/settings.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'settings', component: SettingsComponent },
+  { path: '**', redirectTo: 'dashboard' } // Ruta por defecto
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/features/features.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { SettingsComponent } from './settings/settings.component';
+
+
+
+@NgModule({
+  declarations: [
+    DashboardComponent,
+    SettingsComponent
+  ],
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    DashboardComponent,
+    SettingsComponent
+    ]
+})
+export class FeaturesModule { }
