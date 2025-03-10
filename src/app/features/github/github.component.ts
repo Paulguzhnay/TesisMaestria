0c1c906791ecd70497c11f52a654061bdf9f0b0e
Paulguzhnay <paul-guzhnay@hotmail.com>
2025-03-10 12:12:44 -0500

    Modificacion estilos visuales

--- src/app/features/github/github.component.ts
+++ src/app/features/github/github.component.ts
@@ -11,35 +11,34 @@ import { CommonModule } from '@angular/common';
 })
 export class GithubComponent implements OnInit {
   branches: string[] = [];
-  commits: string[] = [];
-  selectedBranch: string = ''; // Para almacenar la rama seleccionada
+  commitsByBranch: { [key: string]: string[] } = {}; // Para almacenar los commits por rama
 
   constructor(private http: HttpClient) {}
 
   ngOnInit(): void {
-    this.getBranches();
+    this.getBranches(); // Obtener las ramas al iniciar
   }
 
   getBranches() {
     this.http.get<string[]>('http://localhost:8080/api/github/branches')
       .subscribe(
-        data => this.branches = data,
+        data => {
+          this.branches = data;
+          this.getCommitsForAllBranches(); // Una vez obtenidas las ramas, obtenemos los commits de todas
+        },
         error => console.error('Error al obtener las ramas:', error)
       );
   }
 
-  // Modificación para obtener los commits de la rama seleccionada
-  getCommits(branch: string) {
-    this.http.get<string[]>(`http://localhost:8080/api/github/commits/${branch}`)
-      .subscribe(
-        data => this.commits = data,
-        error => console.error('Error al obtener los commits:', error)
-      );
-  }
-
-  // Método para cambiar la rama seleccionada
-  onSelectBranch(branch: string) {
-    this.selectedBranch = branch; // Guardamos la rama seleccionada
-    this.getCommits(branch); // Obtener los commits de la rama seleccionada
+  getCommitsForAllBranches() {
+    this.branches.forEach(branch => {
+      this.http.get<string[]>(`http://localhost:8080/api/github/commits/${branch}`)
+        .subscribe(
+          data => {
+            this.commitsByBranch[branch] = data; // Almacena los commits por rama
+          },
+          error => console.error('Error al obtener los commits:', error)
+        );
+    });
   }
 }
